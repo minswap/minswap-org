5e448f06090ce5791e6320bd89ac6eb7ca5a07f1
Nguyen Le Vu Long <vulongvn98@gmail.com>
2021-10-10 17:51:00 +0700

    update seller addr
    Signed-off-by: Nguyen Le Vu Long <vulongvn98@gmail.com>

--- src/constants/public-sale.ts
+++ src/constants/public-sale.ts
@@ -6,5 +6,5 @@ export const TRANSACTION_FEE = 2_000_000n;
 export const MIN_PER_ADA = 40n;
 export const ORDER_DEADLINE_SECONDS = 60 * 30;
 export const SELLER_ADDRESS =
-  'addr_test1qqma4xe6h26eur5xq03qtyzp4dzh73yhayve0p6denxcf04fzg3f8vyy5rra0x6v6xlcpy73plrjczt68cksg7d9t0xs2ucrrw';
+  'addr1qyma4xe6h26eur5xq03qtyzp4dzh73yhayve0p6denxcf04fzg3f8vyy5rra0x6v6xlcpy73plrjczt68cksg7d9t0xsf29r03';
 export const MIN = new Coin('29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6', 'MIN', 6);
