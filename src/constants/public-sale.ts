07ec6911b0a84b645dacae4ae9d6a5b957f97e68
Nguyen Le Vu Long <vulongvn98@gmail.com>
2021-10-10 16:59:37 +0700

    update seller addr + min policy id
    Signed-off-by: Nguyen Le Vu Long <vulongvn98@gmail.com>

--- src/constants/public-sale.ts
+++ src/constants/public-sale.ts
@@ -6,5 +6,5 @@ export const TRANSACTION_FEE = 2_000_000n;
 export const MIN_PER_ADA = 40n;
 export const ORDER_DEADLINE_SECONDS = 60 * 30;
 export const SELLER_ADDRESS =
-  'addr_test1qquazwmva2j04eh5g603u7n4ztv9tp7l99t6v7zk6hn77hrm6m7xe8g3yylksua66hdvfut677jh5nyahtr6els5ym0sl4pgjs';
-export const MIN = new Coin('497a96fc6f8ce225dc5ecaa07b48fe2f10174a49f908fa4d986c1d9c', 'tMIN', 6);
+  'addr_test1qqma4xe6h26eur5xq03qtyzp4dzh73yhayve0p6denxcf04fzg3f8vyy5rra0x6v6xlcpy73plrjczt68cksg7d9t0xs2ucrrw';
+export const MIN = new Coin('29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6', 'MIN', 6);
