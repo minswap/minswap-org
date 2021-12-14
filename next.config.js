2b24478dfcc63b7c2a6157f68c7573831b8e038f
Khanh Le <lednhatkhanh@gmail.com>
2021-12-14 22:15:59 +0700

    feat: recalculate chart size when resizing

--- next.config.js
+++ next.config.js
@@ -7,23 +7,7 @@ const withTM = require('next-transpile-modules')(['echarts', 'zrender']);
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
 const config = {
-  target: 'serverless',
-  poweredByHeader: false,
   reactStrictMode: true,
-  future: {
-    strictPostcssConfiguration: true,
-  },
-  experimental: {
-    optimizeCss: true,
-  },
-  webpack(webpackConfig) {
-    webpackConfig.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
-
-    // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
-    webpackConfig.experiments = { asyncWebAssembly: true };
-
-    return webpackConfig;
-  },
   images: {
     domains: ['cdn.discordapp.com'],
     deviceSizes: [640, 768, 1024, 1280, 1536],
