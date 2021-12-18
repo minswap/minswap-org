8b90b16ace030937599015cdb7b3ab6e3cbbb54a
Khanh Le <lednhatkhanh@gmail.com>
2021-12-18 22:00:11 +0700

    feat: add cannonical link

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Hydrate } from 'react-query/hydration';
 
@@ -9,6 +10,7 @@ import 'swiper/swiper.scss';
 import 'tippy.js/dist/tippy.css';
 
 function MinswapInterfaceApp({ Component, pageProps }: AppProps): React.ReactElement<AppProps> {
+  const router = useRouter();
   const queryClientRef = React.useRef<QueryClient>();
   if (!queryClientRef.current) {
     queryClientRef.current = new QueryClient();
@@ -21,6 +23,7 @@ function MinswapInterfaceApp({ Component, pageProps }: AppProps): React.ReactEle
         <meta charSet="UTF-8" />
         <meta content="Multi-pool decentralized exchange on Cardano" name="description" />
         <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+        <link href={`${process.env['NEXT_PUBLIC_URL']}${router.pathname}`} rel="cannonical" />
         <meta content="width=device-width, initial-scale=1, viewport-fit=cover" name="viewport" />
 
         {/* Twitter Card Data Start */}
