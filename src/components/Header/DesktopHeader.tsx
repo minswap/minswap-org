0a87f23278cfb44d3bbc33bc28b85042c80f53c6
hieupnh <phannguyenhuyhieu04@gmail.com>
2022-03-11 09:24:02 +0700

    update text

--- src/components/Header/DesktopHeader.tsx
+++ src/components/Header/DesktopHeader.tsx
@@ -15,7 +15,8 @@ export function DesktopHeader({ isScroll, className }: Props) {
   return (
     <div className={classNames(isScroll ? 'sticky top-0 z-50' : null, className)}>
       <Announcement href="https://lbe.minswap.org">
-        🚨 If you participated in the LBE and still haven&#39;t redeemed your PurrADA! Make sure to convert them for the ADA/MIN LPs before March 14th 3:00 AM UTC. If you miss this window your purrADA will become useless.
+        🚨 If you participated in the LBE and still haven&#39;t redeemed your PurrADA! Make sure to convert them for the
+        ADA/MIN LPs before March 14<sup>th</sup> 3:00 AM UTC. If you miss this window your purrADA will become useless.
       </Announcement>
 
       <header className={classNames('grid justify-around py-4 header', isScroll ? 'shadow-md bg-white' : null)}>
